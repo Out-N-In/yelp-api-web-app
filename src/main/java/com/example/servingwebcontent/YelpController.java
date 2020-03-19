38169738f02095c8ba81c212d763ed8f73229c48
Haoyao Chen <taylorchenca@gmail.com>
2020-03-18 19:06:29 -0700

    Added security.

--- src/main/java/com/example/servingwebcontent/YelpController.java
+++ src/main/java/com/example/servingwebcontent/YelpController.java
@@ -6,22 +6,26 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Map;
 
 @Controller
 public class YelpController {
+
     @GetMapping("yelp")
-    public ModelAndView yelp(Map<String, Object> model, RestTemplate restTemplate) {
+    public String yelp(@RequestParam(defaultValue = "ramen") String term, Model model, RestTemplate restTemplate) {
+        StringBuilder sb = new StringBuilder("https://api.yelp.com/v3/businesses/search?term=");
+        sb.append(term);
+        sb.append("&latitude=37.786882&longitude=-122.399972");
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer jiTCndmYGMD0Sm5AGzAijEvAzBKrDltO4_lOBQJ4LrrWvjR2_5VkmR-Cio9c2cbeH9ADRgxX1mn_dz7RDUg9uXF07TJ3gb-dwgYHLU_cOi9gyGjk0i0pKCyH3jlgXnYx");
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<BusinessList> response = restTemplate.exchange("https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972", HttpMethod.GET, entity, BusinessList.class);
-
-        model.put("yelp_result", response.getBody().getBusinesses());
-        return new ModelAndView("yelp");
+        ResponseEntity<BusinessList> response = restTemplate.exchange(sb.toString(), HttpMethod.GET, entity, BusinessList.class);
+        model.addAttribute("yelp_result", response.getBody().getBusinesses());
+        return "yelp";
     }
 }
